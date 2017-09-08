fe4c058b7f2a04b13cce7755f932b2496cd8f81e
Aurelio Montalvo E <aurelio@Mac-mini-de-Aurelio.local>
2017-09-07 22:31:53 -0500

    Plantilla de Registro en el Frontend

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 
 @Component({
@@ -9,9 +10,11 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class RegisterComponent implements OnInit {
     public title: String;
+    public user: User;
 
     constructor(private _route: ActivatedRoute, _router: Router ) {
         this.title = 'Registro'
+        this.user = new User('','','','','','ROLE_USER','');
     }
 
     ngOnInit(){
